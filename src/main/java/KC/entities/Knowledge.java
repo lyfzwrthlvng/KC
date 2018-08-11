4a9ca01b871920fe87e88354e27cd61d9029b5e7
Shrey <shrey.malik@flipkart.com>
2018-08-11 20:37:42 +0530

    Introducing hibernate

---
+++ src/main/java/KC/entities/Knowledge.java
@@ -0,0 +1,45 @@
+package KC.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class Knowledge {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    // The actual knowledge cloud
+    // This id (above) will be associated with multiple
+    // tags, tags are stored separately
+    private String cloud;
+
+    public Knowledge(){
+
+    }
+
+    public Knowledge(Integer id, String cloud) {
+        this.id = id;
+        this.cloud = cloud;
+    }
+
+    public Integer getId() {
+        return this.id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCloud() {
+        return this.cloud;
+    }
+
+    public void setCloud(String cloud) {
+        this.cloud = cloud;
+    }
+}
---
+++ src/main/java/KC/entities/KnowledgeTag.java
@@ -0,0 +1,43 @@
+package KC.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table
+public class KnowledgeTag {
+
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    private String tag;
+
+    public KnowledgeTag(){
+
+    }
+
+    public KnowledgeTag(Integer id, String tag) {
+        this.id = id;
+        this.tag = tag;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+}
